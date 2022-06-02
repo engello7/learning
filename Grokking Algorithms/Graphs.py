777d7b0f05b2c2c4614f8698dfcfdd686781e694
engello7 <engello@inbox.ru>
2022-06-02 22:02:48 +0300

    Added Dijkstra's algorithm

--- Grokking Algorithms/Graphs.py
+++ Grokking Algorithms/Graphs.py
@@ -25,10 +25,63 @@ def searchSeller(name):
                 searchQueue += graph[person]
                 searched.append(person)
     print("nothing is mango seller")
-    return False 
+    return False
+
 
 def personIsSeller(name):
-#    return len(name) < 3
+    #    return len(name) < 3
     return name[-1] == 'm'
 
+
 searchSeller("you")
+
+graphDijkstra = {}
+graphDijkstra["start"] = {}
+graphDijkstra["start"]["a"] = 6
+graphDijkstra["start"]["b"] = 2
+graphDijkstra["a"] = {}
+graphDijkstra["a"]["fin"] = 1
+graphDijkstra["b"] = {}
+graphDijkstra["b"]["a"] = 3
+graphDijkstra["b"]["fin"] = 5
+graphDijkstra["fin"] = {}
+
+infinity = float("inf") 
+costs = {} 
+costs["a"] = 6 
+costs["b"] = 2 
+costs["fin"] = infinity
+
+parents = {} 
+parents["a"] = "start" 
+parents["b"] = "start" 
+parents["fin"] = None 
+
+processed = [] 
+
+def find_lowest_cost_node(costs): 
+    lowest_cost = float("inf") 
+    lowest_cost_node = None 
+    for node in costs:
+        cost = costs[node] 
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node 
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node] 
+    neighbors = graphDijkstra[node] 
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost: 
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node) 
+    node = find_lowest_cost_node(costs)
+
+print(graphDijkstra)
+print(costs)
+print(parents)
+print(costs)
