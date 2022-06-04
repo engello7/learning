62d15102f3ac4477ad510d1f0f26fc0d1ecaf5d7
engello7 <engello@inbox.ru>
2022-06-04 08:56:11 +0300

    Added new examples 2.29

--- course/js/examples_2_29.js
+++ course/js/examples_2_29.js
@@ -1,6 +1,7 @@
 // Место для первой задачи
 function calculateVolumeAndArea(edgeLength) {
-    if (edgeLength == '' || isNaN(edgeLength) || edgeLength == null || edgeLength  < 0 || !(Number.isInteger(edgeLength))) {
+    if (edgeLength == '' || isNaN(edgeLength) || edgeLength == null  || 
+    edgeLength < 0 || !(Number.isInteger(edgeLength))) {
         return "При вычислении произошла ошибка";
     }
     const volume = edgeLength * edgeLength * edgeLength;
@@ -15,6 +16,42 @@ function getCoupeNumber(place) {
     } else if (place == 0 || place > 36) {
         return "Таких мест в вагоне не существует";
     } else {
-        return Math.round((place/4) + 0.25);
+        return Math.round((place / 4) + 0.25);
+    }
+}
+
+// Место для первой задачи
+function getTimeFromMinutes(time) {
+    if (typeof (time) !== 'number' || !Number.isInteger(time) || time < 0) {
+        return "Ошибка, проверьте данные";
+    }
+    let hours = Number.parseInt(time / 60);
+    let minutes = time % 60;
+    if (hours == 1) {
+        return `Это ${hours} час и ${minutes} минут`;
+    } else if (hours == 2 || hours == 3 || hours == 4) {
+        return `Это ${hours} часа и ${minutes} минут`;
+    } else {
+        return `Это ${hours} часов и ${minutes} минут`;
+    }
+}
+
+// Место для второй задачи
+function findMaxNumber(a, b, c, d) {
+    if (typeof (a) !== 'number' || typeof (b) !== 'number' || typeof (c) !== 'number' || typeof (d) !== 'number' || 
+    a == null || b == null || c == null || d == null) {
+        return 0;
+    } else {
+        let max = a;
+        if (max < b) {
+            max = b;
+        }
+        if (max < c) {
+            max = c;
+        }
+        if (max < d) {
+            max = d;
+        }
+        return max;
     }
 }
